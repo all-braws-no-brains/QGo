29cd57aca6f2e9451cf20217a37140011fa92961
Abhyuday Singh <singh.abhyuday.work@gmail.com>
2024-12-28 12:16:53 +0530

    [br-2](full send QGo)

---
+++ pkg/queue/queue.go
@@ -0,0 +1,55 @@
+package queue
+
+import (
+	"errors"
+	"sync"
+)
+
+type Event struct {
+	ID      string
+	Payload []byte
+}
+
+type Queue struct {
+	events []*Event
+	mu     sync.Mutex
+}
+
+func NewQueue() *Queue {
+	return &Queue{
+		events: make([]*Event, 0),
+	}
+}
+
+func (q *Queue) Enqueue(event *Event) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	q.events = append(q.events, event)
+	return nil
+}
+
+func (q *Queue) Dequeue() (*Event, error) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if len(q.events) == 0 {
+		return nil, errors.New("queue is empty")
+	}
+
+	event := q.events[0]
+	q.events = q.events[1:]
+	return event, nil
+}
+
+func (q *Queue) IsEmpty() bool {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	return len(q.events) == 0
+}
+
+func (q *Queue) Size() int {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	return len(q.events)
+}
